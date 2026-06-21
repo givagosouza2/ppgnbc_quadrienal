96c84a881b670fe6d936e66c35c6ffabf77a60f4
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-06-21 16:01:38 -0300

    Update mainv2.py

--- mainv2.py
+++ mainv2.py
@@ -227,11 +227,11 @@ def exibir_video_entrada():
     video_file = Path("videoEntrada.mp4")
     onedrive_url = "https://1drv.ms/v/c/58f7c307dd0b40d5/IQA3PnOTq7oOSaSa-iZ9QFrpAWog0XjOwi8u-qlM0lf5IuE?e=rOp0G2"
     
-    st.markdown("""
-    <div class="video-container">
-        <div class="video-title">🎬 Conheça o PPG</div>
-        <div class="video-wrapper">
-    """, unsafe_allow_html=True)
+    #st.markdown("""
+    #<div class="video-container">
+    #    <div class="video-title">🎬 Conheça o PPG</div>
+    #    <div class="video-wrapper">
+    #""", unsafe_allow_html=True)
     
     if video_file.exists():
         try:
